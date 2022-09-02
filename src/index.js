867cdc8d703efcfef8938a99dfca2db857321e86
usbbush <jeanbienvenusb@gmail.com>
2022-09-02 14:20:39 +0200

    Debug remote version

--- src/index.js
+++ src/index.js
@@ -10,7 +10,7 @@ const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
     <Provider store={store}>
-      <BrowserRouter>
+      <BrowserRouter basename="/aoe2-details">
         <App />
       </BrowserRouter>
     </Provider>
--- src/redux/civilizations/api.js
+++ src/redux/civilizations/api.js
@@ -1,7 +1,7 @@
-// const dataUrl = 'https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations';
+const dataUrl = 'https://age-of-empires-2-api.herokuapp.com/api/v1/civilizations';
 export default async function fetchData() {
   let result = [];
-  await fetch('/api/v1/civilizations', {
+  await fetch(dataUrl, {
     headers: {
       Accept: 'application/json',
       'Access-Control-Allow-Origin': '*',
