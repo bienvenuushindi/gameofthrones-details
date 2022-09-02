6ecc91675ebfb2ba2f3c20b1c54d6b369bf92c00
usbbush <jeanbienvenusb@gmail.com>
2022-09-02 14:10:09 +0200

    Remove console.log

--- src/pages/list.js
+++ src/pages/list.js
@@ -11,7 +11,6 @@ const List = () => {
   const dispatch = useDispatch();
   const states = useSelector((state) => state[slug]) || [];
   // const [loading, setLoading] = useState(true);
-  console.log(states);
   const list = states.map((item, index) => (
     <ListItem
       key={`${slug}-${item.id}`}
