3a025d84923653d604376a78ff6f4a226956d7be
usbbush <jeanbienvenusb@gmail.com>
2022-09-02 15:47:04 +0200

    Update url link

--- src/redux/technologies/api.js
+++ src/redux/technologies/api.js
@@ -1,7 +1,7 @@
-// const dataUrl = 'https://age-of-empires-2-api.herokuapp.com/';
+const dataUrl = 'https://cors-anywhere.herokuapp.com/https://age-of-empires-2-api.herokuapp.com/api/v1/technologies';
 export default async function fetchData() {
   let result = [];
-  await fetch('api/v1/technologies', {
+  await fetch(dataUrl, {
     mode: 'no-cors',
     headers: {
       'Content-Type': 'application/json',
